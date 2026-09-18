fc8e164d635cb5f00c1df0380a0ecc7532fc035f
xxxtrapgh <eastern.39.bokeh@icloud.com>
2026-09-18 06:58:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 const allowedOrigins = [
   'http://127.0.0.1:5000',
   'https://boisterous-queijadas-409afa.netlify.app',
-  'https://rexxiez.netlify.app',
+  'https://trap-way.infy.click',
   'https://bellbusinesscanada.netlify.app',
   'https://sunny-pastelito-8843b7.netlify.app',
   'https://viralbell.netlify.app',
