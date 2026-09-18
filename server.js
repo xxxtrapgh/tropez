e1ba73aeb709d75f18ce074f2f59f8f1d4be9ce9
xxxtrapgh <eastern.39.bokeh@icloud.com>
2026-09-18 07:03:48 +0100

    Replace viralbell.netlify.app with freetraw.ifree.page

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const allowedOrigins = [
   'https://trap-way.infy.click',
   'https://bellbusinesscanada.netlify.app',
   'https://sunny-pastelito-8843b7.netlify.app',
-  'https://viralbell.netlify.app',
+  'https://freetraw.ifree.page',
   'https://jhvnb.vercel.app',
    // add all your trusted frontends here
 ];
